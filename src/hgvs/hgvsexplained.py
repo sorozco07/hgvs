1037540cc16b5fd0ad3dd58e1277aeb94239d13a
rrfreimuth <freimuth.robert@gmail.com>
2023-09-18 12:34:16 -0500

    Update hgvsexplained.py

--- src/hgvs/hgvsexplained.py
+++ src/hgvs/hgvsexplained.py
@@ -10,17 +10,20 @@ class HGVSExplained(object):
 
     """
 
-    def __init__(self ):
-        #self._ 
-        # { orig_var_string:'',
-        #   hgvs_obj:hgvs, 
-        #   hgvs_parser_exception:hgvs_exception, hgvs_error_type:'categorized term',
-        #   parse_explain : [ HGVSExplained ] }
+    def __init__(self, *, orig_var_string, hgvs_obj, hgvs_parser_exc, hgvs_error_type ):
+        self.orig_var_string = orig_var_string
+        self.hgvs_obj = hgvs_obj
+        self.hgvs_parser_exc = hgvs_parser_exc
+        self.hgvs_error_type = hgvs_error_type
+        self.parse_explain = [] # array of HGVSExplained
+
         # if orig var string is valid hgvs
         #   hgvs_obj will be populated
         # else
         #   hgvs_parser_exception and hgvs_error_type will be populated
         #   if attempt rescue, then parse_explain also will be populated
 
-    # TODO: define accessors and convenience methods
+    def add_explained(self, expl):
+        assert( isinstance(expl, HGVSExplained) )
+        self.parse_explain.append(expl)
 
