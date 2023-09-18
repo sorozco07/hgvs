3e86169d19a69f5b9f89569872ef3a2d546ab988
Silvia Orozco <sorozco@uchicago.edu>
2023-09-18 10:16:15 -0700

    adding _explain method

--- src/hgvs/parserexplainer.py
+++ src/hgvs/parserexplainer.py
@@ -5,7 +5,7 @@ to the official HGVS spec (when initial parsing throws an exception).
 
 """
 
-
+import re
 class ParserExplainer(object):
     """Provides ...
 
@@ -27,44 +27,43 @@ class ParserExplainer(object):
 
         self._orig_var_string = v
 
-        # returns HGVSExplained
-
-        # TODO:  move logic from below into specialized methods
-
         try:
             return self.parse_hgvs_variant(v)
         except HGVSParseError as exc:
-            match = re.search( "char (\d+): expected (.+)$", exc.args[0] )
+            self._explain(v, exc)
+        
+    def _explain(self, v, exc):
+        match = re.search( "char (\d+): expected (.+)$", exc.args[0] )
 
-            if( not match ):
-                msg = "[{v}] bombed, cannot handle this error yet: {exc}".format(v=v, exc=exc)
-                raise exc( msg ) # pass the exception through (todo: check syntax)
+        if( not match ):
+            msg = "[{v}] bombed, cannot handle this error yet: {exc}".format(v=v, exc=exc)
+            raise exc( msg ) # pass the exception through (todo: check syntax)
 
-            char_pos = int( match.group(1) )
-            expected_str = match.group(2)
-            #print("got match at [{pos}]".format(pos=char_pos))
+        char_pos = int( match.group(1) )
+        expected_str = match.group(2)
+        #print("got match at [{pos}]".format(pos=char_pos))
 
-            # try to handle diff error types based on the expected_str
-            if( expected_str == "EOF" ):
-                # examples that trigger this error are often caused by a single char, so skip it
-                # todo: consider whether to try to identify offending char(s) dynamically
-                part1, part2 = v[:char_pos], v[char_pos+1:]
-                part2 = part2.strip() # part1 is likely valid, but part2 might have whitespace
-                print( "got an EOF, creating [{part1}], [{part2}]".format(part1=part1, part2=part2))
+        # try to handle diff error types based on the expected_str
+        if( expected_str == "EOF" ):
+            # examples that trigger this error are often caused by a single char, so skip it
+            # todo: consider whether to try to identify offending char(s) dynamically
+            part1, part2 = v[:char_pos], v[char_pos+1:]
+            part2 = part2.strip() # part1 is likely valid, but part2 might have whitespace
+            print( "got an EOF, creating [{part1}], [{part2}]".format(part1=part1, part2=part2))
 
-                # try to parse each half separately
-                v1 = self.parse( part1, explain=True )
-                if( v1 ):
-                    print("rescued and parsed part1:", v1)
-                v2 = self.parse( part2, explain=True )
-                if( v2 ):
-                    print("rescued and parsed part2:", v2)
+            # try to parse each half separately
+            v1 = self.parse( part1, explain=True )
+            if( v1 ):
+                print("rescued and parsed part1:", v1)
+            v2 = self.parse( part2, explain=True )
+            if( v2 ):
+                print("rescued and parsed part2:", v2)
 
-            elif( expected_str == "a digit" ):
-                # checking for 'p.' and '{AA}{\d+}{AA}'
-                m = re.search( "p\..*([a-zA-Z][a-zA-Z]{2}?)(\d+)([a-zA-Z][a-zA-Z]{2}?)", exc.args[0], flags=re.IGNORECASE )
-                if( m ):
-                    print("chunk [{v}] looks like a p. string: [p.][{g1}][{g2}][{g3}]".format(v=v, g1=m.group(1), g2=m.group(2), g3=m.group(3) ) )
-                
-            else:
-                print("got [{v}], expected [{s}]".format(v=v, s=expected_str) )
+        elif( expected_str == "a digit" ):
+            # checking for 'p.' and '{AA}{\d+}{AA}'
+            m = re.search( "p\..*([a-zA-Z][a-zA-Z]{2}?)(\d+)([a-zA-Z][a-zA-Z]{2}?)", exc.args[0], flags=re.IGNORECASE )
+            if( m ):
+                print("chunk [{v}] looks like a p. string: [p.][{g1}][{g2}][{g3}]".format(v=v, g1=m.group(1), g2=m.group(2), g3=m.group(3) ) )
+            
+        else:
+            print("got [{v}], expected [{s}]".format(v=v, s=expected_str) )
