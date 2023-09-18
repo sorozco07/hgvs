9da2ff58145873cbf6293aaef80eb8ea402236cb
Silvia Orozco <sorozco@uchicago.edu>
2023-09-18 11:37:48 -0700

    adding condition for coding and protein transcript

--- src/hgvs/parserexplainer.py
+++ src/hgvs/parserexplainer.py
@@ -6,6 +6,7 @@ to the official HGVS spec (when initial parsing throws an exception).
 """
 
 import re
+import logging
 from hgvs.exceptions import HGVSParseError
 
 class ParserExplainer(object):
@@ -35,19 +36,15 @@ class ParserExplainer(object):
         
     def _explain(self, v, exc):
         match = re.search( "char (\d+): expected (.+)$", exc.args[0] )
-
+        
         if( not match ):
             msg = "[{v}] bombed, cannot handle this error yet: {exc}".format(v=v, exc=exc)
             raise exc( msg ) # pass the exception through (todo: check syntax)
 
-        char_pos = int( match.group(1) )
+        char_pos = int(match.group(1))
         expected_str = match.group(2)
-        #print("got match at [{pos}]".format(pos=char_pos))
-
-        # try to handle diff error types based on the expected_str
-        if( expected_str == "EOF" ):
-            # examples that trigger this error are often caused by a single char, so skip it
-            # todo: consider whether to try to identify offending char(s) dynamically
+    
+        if(expected_str == "EOF" ):
             part1, part2 = v[:char_pos], v[char_pos+1:]
             part2 = part2.strip() # part1 is likely valid, but part2 might have whitespace
             print( "got an EOF, creating [{part1}], [{part2}]".format(part1=part1, part2=part2))
@@ -65,6 +62,24 @@ class ParserExplainer(object):
             m = re.search( "p\..*([a-zA-Z][a-zA-Z]{2}?)(\d+)([a-zA-Z][a-zA-Z]{2}?)", exc.args[0], flags=re.IGNORECASE )
             if( m ):
                 print("chunk [{v}] looks like a p. string: [p.][{g1}][{g2}][{g3}]".format(v=v, g1=m.group(1), g2=m.group(2), g3=m.group(3) ) )
+        
+        elif re.search("one of .+ or a digit$", expected_str):
+            print("'{l}' is not a valid character. Invalid character at position {c} in string {v}.".format(c=char_pos, v=v, l = v[char_pos]))
+            
+            # c_variant_p_variant = r"^(NM.*)(NP.+)"
+            c_variant_p_variant = r"^(NM[^,]+), (NP.+)"
+            if m := re.search(c_variant_p_variant, v):
+                coding = m.group(1)
+                protein = m.group(2)
+                print("Received two groups [{c}, {p}] but expecting input only expected one.".format(c=coding, p = protein))
+                
+                # try to parse each half separately
+                coding_parse = self._hgvs_parser.parse( coding, explain=True )
+                if( coding_parse ):
+                    print("rescued and parsed part1:", coding)
+                protein_parse = self._hgvs_parser.parse( protein, explain=True )
+                if( protein_parse ):
+                    print("rescued and parsed part2:", protein)
             
         else:
             print("got [{v}], expected [{s}]".format(v=v, s=expected_str) )
